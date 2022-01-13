b60f18d8136d3aead1b7722efa13b3fda9ebe876
Sakura <1152770632@qq.com>
2022-01-13 17:16:57 +0800

    1

---
+++ rabbit/src/main/java/com/hq/rabbit/Application.java
@@ -0,0 +1,17 @@
+package com.hq.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lichaojie
+ * @date 2022/1/13 16:51
+ * @ClassName Application
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
